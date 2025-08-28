53146d89186930873e834fa7e19c241e87921660
minjung <togatecomz.mj@gmail.com>
2025-08-28 14:18:27 +0900

    ie11 스타일

--- assets/js/ui/ie11.js
+++ assets/js/ui/ie11.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // 간편인증
     $('.auth-list li input[type="radio"]').on("change", function () {
         var $li = $(this).closest("li");
         $li.siblings().removeClass("selected");
