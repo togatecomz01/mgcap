96928349d3dea8674b955f660a4da903ce49b06a
leesoli0122 <leesoli0122@gmail.com>
2025-10-13 08:58:16 +0900

    헤더랑 푸터 인크루드로 뽑기

--- assets/js/include.js
+++ assets/js/include.js
@@ -5,27 +5,27 @@ $(function(){
     var $this = $(this);
     $this.load(url, function(res, status, xhr){
       if(status === 'error'){
-        $this.html('ë¡œë“œ ì‹¤íŒ¨: ' + xhr.status + ' ' + xhr.statusText + '<br><code>'+url+'</code>');
+        $this.html('ë¡œë“œ ?‹¤?Œ¨: ' + xhr.status + ' ' + xhr.statusText + '<br><code>'+url+'</code>');
       } else {
-        // include ì™„ë£Œ í›„ nav.js ì‹¤í–‰
+        // include ?™„ë£? ?›„ nav.js ?‹¤?–‰
         if (path.indexOf('nav.html') > -1) {
-          // nav.jsê°€ ì´ë¯¸ ë¡œë“œë˜ì–´ ìˆëŠ”ì§€ í™•ì¸
+          // nav.jsê°? ?´ë¯? ë¡œë“œ?˜?–´ ?ˆ?Š”ì§? ?™•?¸
           if (typeof window.mobileMenu === 'undefined') {
             // nav.js ë¡œë“œ
             $.getScript('../../assets/js/ui/com/nav.js', function() {
-              console.log('nav.js ë¡œë“œ ì™„ë£Œ');
+              console.log('nav.js ë¡œë“œ ?™„ë£?');
             });
           } else {
-            // ì´ë¯¸ ë¡œë“œëœ ê²½ìš° í•¨ìˆ˜ë“¤ ì¬ì‹¤í–‰
+            // ?´ë¯? ë¡œë“œ?œ ê²½ìš° ?•¨?ˆ˜?“¤ ?¬?‹¤?–‰
             if (typeof window.mobileMenu === 'function') window.mobileMenu();
             if (typeof window.menuToggle === 'function') window.menuToggle();
             if (typeof window.headerMenu === 'function') window.headerMenu();
           }
           
-          // ìŠ¤í¬ë¡¤ ì´ë²¤íŠ¸ ì¬ë°”ì¸ë”© ì‹œë„
+          // ?Š¤?¬ë¡? ?´ë²¤íŠ¸ ?¬ë°”ì¸?”© ?‹œ?„
           setTimeout(function() {
             if (typeof window.bindScrollEvents === 'function') {
-              console.log('ìŠ¤í¬ë¡¤ ì´ë²¤íŠ¸ ì¬ë°”ì¸ë”© ì‹œë„');
+              console.log('?Š¤?¬ë¡? ?´ë²¤íŠ¸ ?¬ë°”ì¸?”© ?‹œ?„');
               window.bindScrollEvents();
             }
           }, 100);
