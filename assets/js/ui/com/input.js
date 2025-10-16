9d7a03653cebad0d7e9dbe7261f3747482603759
wnsxkai <wnsxkai@gmail.com>
2025-10-16 15:42:39 +0900

    공통 layout 변경

--- assets/js/ui/com/input.js
+++ assets/js/ui/com/input.js
@@ -10,18 +10,20 @@ $(document).ready(function(){
     /**
      * 3자리 수마다 콤마 적용
     **/
-    var commaFormatter = (function() {
-        
+    var commaFormatter = (function () {
         return {
-            format: function(input) {
-                var value = input.value.replace(/[^0-9]/g, ''); // 숫자 이외의 문자 제거
+            format: function (input) {
+                // acc 클래스가 없으면 그냥 종료
+                if (!input.classList.contains('unit')) return;
+    
+                var value = input.value.replace(/[^0-9]/g, ''); // 숫자 이외 제거
                 value = value.replace(/,/g, ''); // 기존 ',' 제거
                 value = value.replace(/\B(?=(\d{3})+(?!\d))/g, ","); // 3자리마다 ',' 추가
+    
                 input.value = value;
             }
         };
     })();
-
     // event
     $(document).on('keyup', 'input[inputmode=numeric]', function() {
         commaFormatter.format(this);
