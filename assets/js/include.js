ad1166a25c165b2d43f8fdbb1926135d28136327
wnsxkai <wnsxkai@gmail.com>
2025-10-16 15:35:02 +0900

    manager 레이아웃 수정

--- assets/js/include.js
+++ assets/js/include.js
@@ -5,27 +5,27 @@ $(function(){
     var $this = $(this);
     $this.load(url, function(res, status, xhr){
       if(status === 'error'){
-        $this.html('ë¡œë“œ ?‹¤?Œ¨: ' + xhr.status + ' ' + xhr.statusText + '<br><code>'+url+'</code>');
+        $this.html('ë¡œë“œ ?ï¿½ï¿½?ï¿½ï¿½: ' + xhr.status + ' ' + xhr.statusText + '<br><code>'+url+'</code>');
       } else {
-        // include ?™„ë£? ?›„ nav.js ?‹¤?–‰
-        if (path.indexOf('nav.html') > -1) {
-          // nav.jsê°? ?´ë¯? ë¡œë“œ?˜?–´ ?ˆ?Š”ì§? ?™•?¸
+        // include ?ï¿½ï¿½ï¿½? ?ï¿½ï¿½ nav.js ?ï¿½ï¿½?ï¿½ï¿½
+        if (path.indexOf('nav.html') > -1 || path.indexOf('lnb.html') > -1) {
+          // nav.jsï¿½? ?ï¿½ï¿½ï¿½? ë¡œë“œ?ï¿½ï¿½?ï¿½ï¿½ ?ï¿½ï¿½?ï¿½ï¿½ï¿½? ?ï¿½ï¿½?ï¿½ï¿½
           if (typeof window.mobileMenu === 'undefined') {
             // nav.js ë¡œë“œ
             $.getScript('../../assets/js/ui/com/nav.js', function() {
-              console.log('nav.js ë¡œë“œ ?™„ë£?');
+              console.log('nav.js ë¡œë“œ ?ï¿½ï¿½ï¿½?');
             });
           } else {
-            // ?´ë¯? ë¡œë“œ?œ ê²½ìš° ?•¨?ˆ˜?“¤ ?¬?‹¤?–‰
+            // ?ï¿½ï¿½ï¿½? ë¡œë“œ?ï¿½ï¿½ ê²½ìš° ?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½ ?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½
             if (typeof window.mobileMenu === 'function') window.mobileMenu();
             if (typeof window.menuToggle === 'function') window.menuToggle();
             if (typeof window.headerMenu === 'function') window.headerMenu();
           }
           
-          // ?Š¤?¬ë¡? ?´ë²¤íŠ¸ ?¬ë°”ì¸?”© ?‹œ?„
+          // ?ï¿½ï¿½?ï¿½ï¿½ï¿½? ?ï¿½ï¿½ë²¤íŠ¸ ?ï¿½ï¿½ë°”ì¸?ï¿½ï¿½ ?ï¿½ï¿½?ï¿½ï¿½
           setTimeout(function() {
             if (typeof window.bindScrollEvents === 'function') {
-              console.log('?Š¤?¬ë¡? ?´ë²¤íŠ¸ ?¬ë°”ì¸?”© ?‹œ?„');
+              console.log('?ï¿½ï¿½?ï¿½ï¿½ï¿½? ?ï¿½ï¿½ë²¤íŠ¸ ?ï¿½ï¿½ë°”ì¸?ï¿½ï¿½ ?ï¿½ï¿½?ï¿½ï¿½');
               window.bindScrollEvents();
             }
           }, 100);
