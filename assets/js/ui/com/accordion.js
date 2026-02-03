7724546bc9181a4e5af0fe724e6eb2aa0eeb3fe1
wnsxkai <wnsxkai@gmail.com>
2026-02-03 11:00:48 +0900

    버튼에 열기 닫기 위치 이동

--- assets/js/ui/com/accordion.js
+++ assets/js/ui/com/accordion.js
@@ -7,11 +7,15 @@ $(document).ready(function(){
         return {
             open: function(target) {
                 target.addClass("on");
-                target.attr("title", "닫기");
+                //target.attr("title", "닫기");
+                target.find("> .tit button").attr("title", "닫기");//26.2.3 웹접근성
+
+            
             },
             close: function(target) {
                 target.removeClass("on");
-                target.attr("title", "열기");
+                //target.attr("title", "열기");
+                target.find("> .tit button").attr("title", "열기");//26.2.3 웹접근성
             },
             toggle: function(target) {
                 if (target.hasClass("on")) {
